d7b51c131b239ee189f288b3da4224efe165af00
pmh-only <pmhstudio.pmh@gmail.com>
2020-04-02 20:05:35 +0900

    Map 6

--- maps/6.js
+++ maps/6.js
@@ -1,8 +1,12 @@
 /**
  * @param {import('../router/sessions').data} playerData
  */
-function fnc (event, _mapData, playerData, solve) {
-  /* 아직 덜만듬 */
+function fnc (_event, _mapData, playerData, solve) {
+  const filter = (m) => (m.x + m.y) % 2 === 0
+  const found = playerData.mark[6].find(filter)
+
+  if (found) playerData.mark[6].splice(0, 9999)
+  else if (playerData.mark[6].length > 16) solve()
 }
 
 module.exports = fnc
